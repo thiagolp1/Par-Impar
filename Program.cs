c497fa96321b0d612e1af1dc49c0908628b715eb
Thiago <thiagolopespereiralp1@gmail.com>
2022-04-01 20:38:50 -0300

    finalização do exercicio

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int numerod;
+
+Console.Write("Digite um número: ");
+numerod = Convert.ToInt32(Console.ReadLine());
+
+bool  numeropar = (numerod % 2)==0;
+if (numeropar)
+{
+       Console.WriteLine($"{numerod} Eh par"); 
+       Console.ForegroundColor = ConsoleColor.Red;
+       Console.Beep(); Console.Beep(); 
+}  
+else  
+{
+       Console.WriteLine($"{numerod} Eh impar");
+       Console.ForegroundColor = ConsoleColor.Blue;
+       Console.Beep();
+}       
+Console.ResetColor();
